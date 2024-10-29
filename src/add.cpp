9856e7cba8c1408f0ca47aae0daff5671c9d0dfe
ndifix <yoshiki.linuxmint@gmail.com>
2024-10-29 16:02:03 +0900

    配列の加算を追加

--- src/add.cpp
+++ src/add.cpp
@@ -4,4 +4,17 @@ int add_int(int a, int b) {
     return a + b;
 }
 
+void
+vector_add_int (
+    const int* a,
+    const int* b,
+    int* c,
+    int n
+    )
+{
+    for (int i = 0; i < n; i++) {
+        c[i] = a[i] + b[i];
+    }
+}
+
 }
--- src/cpp.rs
+++ src/cpp.rs
@@ -1,6 +1,9 @@
 extern "C" {
     #[allow(dead_code)]
     fn add_int(a: i32, b: i32) -> i32;
+
+    #[allow(dead_code)]
+    fn vector_add_int(a: *const i32, b: *const i32, c: *mut i32, n: i32);
 }
 
 #[cfg(test)]
@@ -9,5 +12,18 @@ mod test {
     fn add_int_1() {
         let result = unsafe { super::add_int(1, 2) };
         assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn vector_add_int_1() {
+        let a = [0, 1, 2, 3];
+        let b = [2, 4, 6, 8];
+        let mut c = [0, 0, 0, 0];
+        let expect = [2, 5, 8, 11];
+        unsafe {
+            super::vector_add_int(a.as_ptr(), b.as_ptr(), c.as_mut_ptr(), 4);
+        }
+
+        assert_eq!(c, expect);
     }
 }
