3d0578942717a1839f4cb287323950a8745cfa5b
ndifix <yoshiki.linuxmint@gmail.com>
2024-11-02 20:38:39 +0900

    畳み込みを追加

--- src/cuda.rs
+++ src/cuda.rs
@@ -4,6 +4,8 @@ extern "C" {
     fn mul2_cuda(xs: *mut i32, n: usize);
     #[allow(dead_code)]
     fn mul2_cuda_copy(xs: *const i32, ys: *mut i32, n: usize);
+    #[allow(dead_code)]
+    fn mul_cuda(a: *const i32, b: *const i32, c: *mut i32, n: usize);
 }
 
 #[cfg(test)]
@@ -29,5 +31,18 @@ mod test {
         }
 
         assert_eq!(b, expect);
+    }
+
+    #[test]
+    fn vector_mul_int_cuda_1() {
+        let a = [0, 1, 2, 3];
+        let b = [5, 3, 4, 2];
+        let mut c = [0; 4];
+        let expect = [0, 3, 8, 6];
+        unsafe {
+            super::mul_cuda(a.as_ptr(), b.as_ptr(), c.as_mut_ptr(), 4);
+        }
+
+        assert_eq!(c, expect);
     }
 }
--- src/mul.cu
+++ src/mul.cu
@@ -53,3 +53,45 @@ mul2_cuda_copy (
     cudaFree(dev_xs);
     cudaDeviceReset();
 }
+
+__global__ void
+mul_cuda_impl (
+    int *a,
+    int *b,
+    int *c,
+    const size_t n
+    )
+{
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < n) {
+        c[i] = a[i] * b[i];
+    }
+}
+
+extern "C" void
+mul_cuda (
+    int *as,
+    int *bs,
+    int *cs,
+    const size_t n
+    )
+{
+    size_t bytes = n * sizeof(int);
+
+    int *dev_as, *dev_bs, *dev_cs;
+    cudaMalloc(&dev_as, bytes);
+    cudaMalloc(&dev_bs, bytes);
+    cudaMalloc(&dev_cs, bytes);
+    cudaMemcpy(dev_as, as, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_bs, bs, bytes, cudaMemcpyHostToDevice);
+
+    dim3 block(1024);
+    dim3 grid((n + block.x - 1) / block.x);
+    mul_cuda_impl<<<grid, block>>>(dev_as, dev_bs, dev_cs, n);
+
+    cudaMemcpy(cs, dev_cs, bytes, cudaMemcpyDeviceToHost);
+    cudaFree(dev_as);
+    cudaFree(dev_bs);
+    cudaFree(dev_cs);
+    cudaDeviceReset();
+}
